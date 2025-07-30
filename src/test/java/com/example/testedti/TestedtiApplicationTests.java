b0223e71db6630870b42c6082fd0523e1c272478
ÃIago Wilian <agowillian22@gmail.com>
2025-07-30 15:44:59 -0300

    criando o projeto

---
+++ src/test/java/com/example/testedti/TestedtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testedti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestedtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
